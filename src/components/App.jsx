58757f03f6c09d5b3aa252f849b15a45114b6ab3
Sarang Wadode <sarangwadode@gmail.com>
2022-01-09 00:50:04 +0530

    basic css applied

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,63 +8,63 @@ const keys = [
     'class': 'drum-pad',
     'dataKey': '81',
     'text': 'Q',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    'sound': './sounds/Heater-1.mp3',
     'name': 'Heater 1'
   },
   {
     'class': 'drum-pad',
     'dataKey': '87',
     'text': 'W',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+    'sound': './sounds/Heater-2.mp3',
     'name': 'Heater 2'
   },
   {
     'class': 'drum-pad',
     'dataKey': '69',
     'text': 'E',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    'sound': './sounds/Heater-3.mp3',
     'name': 'Heater 3'
   },
   {
     'class': 'drum-pad',
     'dataKey': '65',
     'text': 'A',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+    'sound': './sounds/Heater-4_1.mp3',
     'name': 'Heater 4'
   },
   {
     'class': 'drum-pad',
     'dataKey': '83',
     'text': 'S',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+    'sound': './sounds/Heater-6.mp3',
     'name': 'Clap'
   },
   {
     'class': 'drum-pad',
     'dataKey': '68',
     'text': 'D',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    'sound': './sounds/Dsc_Oh.mp3',
     'name': 'Open HH'
   },
   {
     'class': 'drum-pad',
     'dataKey': '90',
     'text': 'Z',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    'sound': './sounds/Kick_n_Hat.mp3',
     'name': 'Kick n\' Hat'
   },
   {
     'class': 'drum-pad',
     'dataKey': '88',
     'text': 'X',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    'sound': './sounds/RP4_KICK_1.mp3',
     'name': 'Kick'
   },
   {
     'class': 'drum-pad',
     'dataKey': '67',
     'text': 'C',
-    'sound': 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    'sound': './sounds/Cev_H2.mp3',
     'name': 'Closed HH'
   }
 ]
